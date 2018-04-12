09797fa5c6f922f77ca99b3827522fd3bd9ba81a
Luca Naterop <luca.naterop@bluewin.ch>
2018-04-12 18:34:40 +0200

    stuff

--- spacedreps-app/client/collections.js
+++ spacedreps-app/client/collections.js
@@ -1,6 +1,6 @@
 
-// backendURL = 'http://159.89.16.187:3000/';
-backendURL = 'http://localhost:3000';
+backendURL = 'http://159.89.16.187:3000/';
+// backendURL = 'http://localhost:3000';
 
 var conn = DDP.connect(backendURL);
 
